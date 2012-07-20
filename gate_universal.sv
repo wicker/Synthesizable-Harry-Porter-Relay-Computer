ad135b18f560b3e8a1d5bb8bd7bd88a7fc031693
Jenner Hanni <jeh.wicker@gmail.com>
2012-07-20 12:12:53 -0700

    Fix syntax.

--- gate_universal.sv
+++ gate_universal.sv
@@ -5,47 +5,46 @@
  * License: MIT http://opensource.org/licenses/MIT
 */
 
-module GateBlock()
+module GateBlock(b, c, NOT, OR, AND, XOR)
 
-  wire a, b, g, h, f, NOT, OR, AND, XOR;
+  wire b, c, g, h, f, NOT, OR, AND, XOR;
 
-  module Relay relay1 (input control(b),
-                      inout in_1(V),
-                      inout in_2(V),
-                      inout in_3(V),
-                      inout out_hi_1(g),
-                      inout out_hi_3(NOT),
-                      inout out_lo_1(h),
-                      inout out_lo_2(OR),
-                      inout out_lo_3(f) );
+  Relay relay1 (.control(b),
+                in_1(V),
+                in_2(V),
+                in_3(V),
+                out_hi_1(g),
+                out_hi_3(NOT),
+                out_lo_1(h),
+                out_lo_2(OR),
+                out_lo_3(f) );
 
-  module Relay relay2 (input control(c),
-                      inout in_1(AND),
-                      inout in_1(V),
-                      inout in_2(XOR),
-                      inout out_hi_2(h),
-                      inout out_hi_3,
-                      inout out_lo_0(f),
-                      inout out_lo_1(OR),
-                      inout out_lo_2(g) );
+  Relay relay2 (control(c),
+                in_1(AND),
+                in_1(V),
+                in_2(XOR),
+                out_hi_2(h),
+                out_lo_0(f),
+                out_lo_1(OR),
+                out_lo_2(g) );
 
   always_comb begin
-    if (a == 0 & b == 0) begin
+    if (b == 0 & c == 0) begin
        AND = 0;
        OR  = 0;
        XOR = 0;
        NOT = 0;
-    if (a == 0 & b == 1) begin
+    if (b == 0 & c == 1) begin
        AND = 0;
        OR  = 1;
        XOR = 1;
        NOT = 0;
-    if (a == 1 & b == 0) begin
+    if (b == 1 & c == 0) begin
        AND = 0;
        OR  = 1;
        XOR = 1;
        NOT = 1;
-    if (a == 1 & b == 1) begin
+    if (b == 1 & c == 1) begin
        AND = 1;
        OR  = 1;
        XOR = 0;
