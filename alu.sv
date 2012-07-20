92447962b52c6eb5b8bbb8a2abb76ca57386e6c3
wicker <jeh.wicker@gmail.com>
2012-07-20 14:59:42 -0700

    Clean up ALU top-level file.

---
+++ alu.sv
@@ -0,0 +1,17 @@
+/* Author: Jenner Hanni
+ * Project: Harry Porter Relay Computer
+ * File: The ALU, which uses eight 1-bit logic blocks which
+ * can compute logical AND, OR, XOR, and NOT on all 8 bits.
+ * License: MIT http://opensource.org/licenses/MIT
+*/
+
+module ALU (input [7:0] b, c, output [7:0] result);
+
+  wire AND, OR, XOR, NOT;
+  reg AND_var, OR_var, XOR_var, NOT_var;
+
+  // 
+  // 3-to-8 Function Decoder
+  // 
+ 
+endmodule
