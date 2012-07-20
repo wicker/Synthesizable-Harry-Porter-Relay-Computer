9d51cb1252c3543ca68576fff8fe2569e4ee95e0
wicker <jeh.wicker@gmail.com>
2012-07-20 12:59:56 -0700

    Testbench compiles.

--- gate_universal_testbench.sv
+++ gate_universal_testbench.sv
@@ -1,57 +1,58 @@
 /* Author: Jenner Hanni
  * Project: Harry Porter Relay Computer
- * File: 1-bit logic block implements four logic functions at once
- * It was designed in such a way so backfeed cannot occur. Testbench. 
+ * File: Testbench for 1-bit logic block which implements four logic 
+ * functions at once.
  * License: MIT http://opensource.org/licenses/MIT
 */
 
 module UniversalGateTestBench;
 
-  UniversalGate GateBlock();
+  UniversalGate GateBlock( b, c, NOT, OR, AND, XOR );
+ 
+  reg Clock, Clear, b_var, c_var;
 
-  wire a, b, g, h, f, NOT, OR, AND, XOR;
+  //
+  // set up monitor
+  // 
 
-  module Relay relay1 (input control(b),
-                      inout in_1(V),
-                      inout in_2(V),
-                      inout in_3(V),
-                      inout out_hi_1(g),
-                      inout out_hi_3(NOT),
-                      inout out_lo_1(h),
-                      inout out_lo_2(OR),
-                      inout out_lo_3(f) );
+  initial begin
+    $display("b   c    NOT   OR    AND    XOR");
+    forever @(posedge Clock) begin
+      #1
+      $display("%b %b %b %b %b %bi",b,c,NOT,OR,AND,XOR);
+    end
+  end
 
-  module Relay relay2 (input control(c),
-                      inout in_1(AND),
-                      inout in_1(V),
-                      inout in_2(XOR),
-                      inout out_hi_2(h),
-                      inout out_hi_3,
-                      inout out_lo_0(f),
-                      inout out_lo_1(OR),
-                      inout out_lo_2(g) );
+  // 
+  // Create free running clock
+  // 
 
-  always_comb begin
-    if (a == 0 & b == 0) begin
-       AND = 0;
-       OR  = 0;
-       XOR = 0;
-       NOT = 0;
-    if (a == 0 & b == 1) begin
-       AND = 0;
-       OR  = 1;
-       XOR = 1;
-       NOT = 0;
-    if (a == 1 & b == 0) begin
-       AND = 0;
-       OR  = 1;
-       XOR = 1;
-       NOT = 1;
-    if (a == 1 & b == 1) begin
-       AND = 1;
-       OR  = 1;
-       XOR = 0;
-       NOT = 1;
-   end
+  initial begin 
+    Clock = 0;
+    forever #10ms Clock = ~Clock;
+  end
+
+  // 
+  // Continuous assign statement
+  // 
+
+  assign b = b_var;
+  assign c = c_var;
+
+  // 
+  // Generate combinations after waiting for reset
+  // 
+
+  initial begin
+    Clear = 1;
+    repeat (2) @(negedge Clock);
+    Clear = 0;
+
+    repeat (1) @(negedge Clock); {b_var,c_var} = 2'b00;
+    repeat (1) @(negedge Clock); {b_var,c_var} = 2'b01;
+    repeat (1) @(negedge Clock); {b_var,c_var} = 2'b10;
+    repeat (1) @(negedge Clock); {b_var,c_var} = 2'b11;
+   
+  end
 
 endmodule
