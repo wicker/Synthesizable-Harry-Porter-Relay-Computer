491fe7fe26065e68254aa70ff860035ffc0ec949
wicker <jeh.wicker@gmail.com>
2012-07-28 16:32:50 -0700

    ALU skeleton.

--- alu.sv
+++ alu.sv
@@ -5,24 +5,41 @@
  * License: MIT http://opensource.org/licenses/MIT
 */
 
-module ALU (input logic [7:0] b, c, output logic [7:0] result);
+module ALU (input logic [7:0] b, c, output logic [7:0] alu_result, 
+                                    output logic sign, carry, zero);
 
-ThreeToEightDecoder three_to_eight ();
+  ThreeToEightDecoder op_decoder (input logic [2:0] fctn_code, 
+                                  output logic [7:0] op);
 
-EightBitAdderUnit adder (input b, c, output op);
+  EightBitAdderUnit adder (input logic [7:0] b, c, 
+                           output logic [7:0] add, inc, 
+                           output logic carry);
 
-EightBitLogicUnit logic (input b, c, output op);
+  EightBitLogicUnit logic (input logic [7:0] b, c, 
+                           output logic [7:0] and, or, xor, not);
 
-Enable enable_add ();
-Enable enable_inc ();
-Enable enable_and ();
-Enable enable_or ();
-Enable enable_xor ();
-Enable enable_not ();
-Enable enable_shl ();
+  ShiftLeft shift_left (input logic [7:0] b, c, 
+                        output logic [7:0] shl);
 
-ResultBus result_bus (output result);
+  Enable enable_add (input logic en_add, input logic [7:0] add, 
+                     output [7:0] result);
+  Enable enable_inc (input logic en_inc, input logic [7:0] inc,
+                     output [7:0] result);
+  Enable enable_and (input logic en_and, input logic [7:0] and, 
+                     output [7:0] result);
+  Enable enable_or (input logic en_or, input logic [7:0] or, 
+                     output [7:0] result);
+  Enable enable_xor (input logic en_xor, input logic [7:0] xor, 
+                     output [7:0] result);
+  Enable enable_not (input logic en_not, input logic [7:0] not, 
+                     output [7:0] result);
+  Enable enable_shl (input logic en_shl, input logic [7:0] shl, 
+                     output [7:0] result);
 
-ZeroDetect zero_detector ();
+  ResultBus result_bus (input logic [7:0] result,
+                        output logic [7:0] alu_result);
+
+  ZeroDetect zero_detector (input logic [7:0] result,
+                            output logic zero);
 
 endmodule
