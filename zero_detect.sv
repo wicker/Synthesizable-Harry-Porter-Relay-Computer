9271ab09b604203a1ea6fc64b27d30522c8c7ccf
wicker <jeh.wicker@gmail.com>
2012-07-28 21:22:12 -0700

    More trouble in testbench because of confusion with nets and wires.

--- zero_detect.sv
+++ zero_detect.sv
@@ -1,7 +1,7 @@
 /* Author: Jenner Hanni
  * Project: Harry Porter Relay Computer
  * File: Zero Detect Function in the ALU
- *       This module detects when the result is all zeros.
+ * This module detects when the result is all zeros.
  * License: MIT http://opensource.org/licenses/MIT
 */
 
@@ -10,36 +10,37 @@ module ZeroDetect (input logic [7:0] result, output logic zero);
   logic f,g,h,i,j,k,l,m;
   logic V = 1;
 
-  Relay relay0 (.control(r[0]),
+  Relay relay0 (.control(result[0]),
                 .in_3(V),
                 .out_lo_3(f));
 
-  Relay relay1 (.control(r[1]),
+  Relay relay1 (.control(result[1]),
                 .in_3(f),
                 .out_lo_3(g));
 
-  Relay relay2 (.control(r[2]),
+  Relay relay2 (.control(result[2]),
                 .in_3(g),
                 .out_lo_3(h));
 
-  Relay relay3 (.control(r[3]),
+  Relay relay3 (.control(result[3]),
                 .in_3(h),
                 .out_lo_3(i));
 
-  Relay relay4 (.control(r[4]),
+  Relay relay4 (.control(result[4]),
                 .in_3(i),
                 .out_lo_3(k));
 
-  Relay relay5 (.control(r[5]),
+  Relay relay5 (.control(result[5]),
                 .in_3(k),
                 .out_lo_3(l));
 
-  Relay relay6 (.control(r[6]),
+  Relay relay6 (.control(result[6]),
                 .in_3(l),
                 .out_lo_3(m));
 
-  Relay relay7 (.control(r[7]),
+  Relay relay7 (.control(result[7]),
                 .in_3(m),
                 .out_lo_3(zero) );
 
 endmodule
+
