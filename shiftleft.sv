2b3eb1e2e8377586f830bdd62a6266048ab8c6fa
wicker <jeh.wicker@gmail.com>
2012-07-28 20:38:57 -0700

    Build Left Shift Unit functionality.

--- shiftleft.sv
+++ shiftleft.sv
@@ -4,38 +4,41 @@
  * License: MIT http://opensource.org/licenses/MIT
 */
 
-module ShiftLeftCircular (input logic [7:0] b, output logic [7:0] shl);
+module ShiftLeftCircular (input logic [7:0] b, input logic en_shl,
+                          output logic [7:0] shl);
 
-  Relay relay1 (.control(f1),
-                .in_3(V),
-                .out_hi_3(f),
-                .out_lo_3(g));
+  logic r0,r1,r2,r3,r4,r5,r6,r7;
 
-  Relay relay2 (.control(f2),
-                .in_2(f),
-                .in_3(g),
-                .out_hi_2(h),
-                .out_hi_3(i),
-                .out_lo_2(j),
-                .out_lo_3(k) );
+  Relay relay1 (.control(en_shl),   // b0-b3
+                .in_0(b0),
+                .in_1(b1),
+                .in_2(b2),
+                .in_3(b3),
+                .out_hi_0(),
+                .out_hi_1(),
+                .out_hi_2(),
+                .out_hi_3(),
+                .out_lo_0(r1),
+                .out_lo_1(r2),
+                .out_lo_2(r3),
+                .out_lo_3(r4));
 
-  Relay relay3 (.control(f3),
-                .in_0(h),
-                .in_1(i),
-                .in_2(j),
-                .in_3(k),
-                .out_hi_0(ADD_pin),
-                .out_hi_1(INC_pin),
-                .out_hi_2(AND_pin),
-                .out_hi_3(OR_pin),
-                .out_lo_0(XOR_pin),
-                .out_lo_1(NOT_pin),
-                .out_lo_2(SHIFTL_pin),
-                .out_lo_3(NULL_pin) );
+  Relay relay2 (.control(en_shl),   // b4-b7
+                .in_0(b4),
+                .in_1(b5),
+                .in_2(b6),
+                .in_3(b7),
+                .out_hi_0(),
+                .out_hi_1(),
+                .out_hi_2(),
+                .out_hi_3(),
+                .out_lo_0(r5),
+                .out_lo_1(r6),
+                .out_lo_2(r7),
+                .out_lo_3(r7));
 
   always_comb begin
-    V_var = 1;
-    logic [7:0] = {ADD_pin, INC_pin, 
-   end
+    shl = {r7,r6,r5,r4,r3,r2,r1,r0};
+  end
 
 endmodule
