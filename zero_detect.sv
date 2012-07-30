48a97456463e44acb24b15a89bbd24546c04bf21
wicker <jeh.wicker@gmail.com>
2012-07-30 11:26:28 -0700

    HELP: failure on line 26 when simulated.

--- zero_detect.sv
+++ zero_detect.sv
@@ -7,40 +7,55 @@
 
 module ZeroDetect (input logic [7:0] result, output logic zero);
 
-  logic f,g,h,i,j,k,l,m;
+  wire f,g,h,i,j,k,l,m;
+  wire r0,r1,r2,r3,r4,r5,r6,r7,zero_pin;
   logic V = 1;
 
-  Relay relay0 (.control(result[0]),
+  assign r0 = result[0];
+  assign r1 = result[1];
+  assign r2 = result[2];
+  assign r3 = result[3];
+  assign r4 = result[4];
+  assign r5 = result[5];
+  assign r6 = result[6];
+  assign r7 = result[7];
+  assign zero = zero_pin;
+
+  Relay relay0 (.control(r0),
                 .in_3(V),
                 .out_lo_3(f));
 
-  Relay relay1 (.control(result[1]),
+  Relay relay1 (.control(r1),
                 .in_3(f),
                 .out_lo_3(g));
 
-  Relay relay2 (.control(result[2]),
+  Relay relay2 (.control(r2),
                 .in_3(g),
                 .out_lo_3(h));
 
-  Relay relay3 (.control(result[3]),
+  Relay relay3 (.control(r3),
                 .in_3(h),
                 .out_lo_3(i));
 
-  Relay relay4 (.control(result[4]),
+  Relay relay4 (.control(r4),
                 .in_3(i),
                 .out_lo_3(k));
 
-  Relay relay5 (.control(result[5]),
+  Relay relay5 (.control(r5),
                 .in_3(k),
                 .out_lo_3(l));
 
-  Relay relay6 (.control(result[6]),
+  Relay relay6 (.control(r6),
                 .in_3(l),
                 .out_lo_3(m));
 
-  Relay relay7 (.control(result[7]),
+  Relay relay7 (.control(r7),
                 .in_3(m),
-                .out_lo_3(zero) );
+                .out_lo_3(zero_pin) );
+
+  always @(result) begin
+    #1; 
+  end
 
 endmodule
 
