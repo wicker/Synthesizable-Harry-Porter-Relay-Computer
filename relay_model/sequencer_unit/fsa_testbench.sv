c3a44fde7e530862d67e624308d4e37fe59f7259
wicker <jeh.wicker@gmail.com>
2012-08-10 16:04:31 -0700

    Not perfect but structure and wires are there. Possible high low mixup.

---
+++ relay_model/sequencer_unit/fsa_testbench.sv
@@ -0,0 +1,23 @@
+/* Author: Jenner Hanni
+ * Project: Harry Porter Relay Computer
+ * File: FSA Testbench
+ * License: MIT http://opensource.org/licenses/MIT
+*/
+
+module FSATestBench ();
+
+  logic clock, reset;
+  wire logic [2:0] fsm_out;
+
+  FSA sequencer (clock, reset, fsm_out);
+
+  initial begin
+    forever #1ms clock = ~clock;
+  end
+
+  initial begin
+    $display("fsm = %b",fsm_out);
+  $stop;
+  end
+
+endmodule
