4ecbb58641ca2e1da1d8ebbb654b4d240493b8ec
wicker <jeh.wicker@gmail.com>
2012-08-10 16:50:24 -0700

    It's like a map.

--- relay_model/sequencer_unit/instruction_decoder.sv
+++ relay_model/sequencer_unit/instruction_decoder.sv
@@ -4,37 +4,27 @@
  * License: MIT http://opensource.org/licenses/MIT
 */
 
-module InstructionDecoder (input logic clock, [1:24] inst_reg, [7:0] fsa_in, 
+module InstructionDecoder (input logic clock, [1:24] inst_reg, [23:0] fsa_in, 
                            output [33:0] inst_decoder_out);
 
-// This module receives input from the instruction register and the FSA
-// in the physical computer it lights LEDs to indicate the correct instruction
+// this module contains fifty relays broken up into eight blocks
+// cond code register, fetch-incr, derived pulses, mov8/mov16, goto, 
+// instdecode/setab/inc16/alu, and two unnamed
 
-// Instructions:
-// MOV-8
-// ALU
-// SETAB
-// INC-XY
-// LOAD
-// STORE
-// MOV-16: General
-// MOV-16: Halt
-// MOV-16: Return/Branch-indirect Instruction
-// GOTO: General
-// GOTO Variant: SETM 
-// GOTO Variant: JUMP
-// GOTO Variant: CALL
-// GOTO Variant: BNEG
-// GOTO Variant: BNC
-// GOTO Variant: BZERO
-// GOTO Variant: BNZ
+// inputs include nineteen derived pulses pA through pT
+// and one byte from the instruction register
+
+// outputs to the 43 leds
+  
+// condition code register: r1,r2,r3,r11,r12,r13
+// fetch-increment: r4,r5,r6,r7
+// derived pulses: r8,r9,r10
+// inst decode/alu/other: r21, r22, r23, r24, r31, r32, r33, r34, r41, r42, r43, r44
+// unknown: r14, r15, r25, r35, r45
+// goto: r16, r17, r26, r27, r36, r37, r46, r47, r48
+// mov8/mov16: r18, r19, r20, r29, r30, r39, r40
+// unknown: r49, f50
 
 
-  initial begin
-  end
-
-
-  always @() begin
-  end
 
 endmodule
