45b16cf01a52fef11f74be9749bfbfea10b38b82
wicker <jeh.wicker@gmail.com>
2012-08-13 07:49:06 -0700

    Control bus built with register modports.

--- relay_model/bus_ctrl.sv
+++ relay_model/bus_ctrl.sv
@@ -6,44 +6,47 @@
 
 interface Ctrl_Bus();
 
-  parameter CTRL_BUS_WIDTH = 34;
-
   wire  ldA, ldB, ldC, ldD, ldINST, ldM1, ldM2, ldM, 
-        ldX, ldY, ldXY, ldJ1, ldJ2, ldPC, ldINC, ldCCR;
+        ldX, ldY, ldXY, ldJ1, ldJ2, ldPC, ldINC, ldCCR,
+        mem_write;
 
   wire  selA, selB, selC, selD, selM1, selM2, selM, 
-        selX, selY, selXY, selJ, selPC, selINC;
+        selX, selY, selXY, selJ, selPC, selINC, mem_read;
 
-  modport decoder   (output ldA, selA);
+  wire  bus_to_mem;
 
-  modport reg_B     (input wire load, sel, clock);
+  modport reg_A (input ldA, selA);
 
-  modport reg_C     (input wire load, sel, clock);
+  modport reg_B (input ldB, selB);
 
-  modport reg_D     (input wire load, sel, clock);
+  modport reg_C (input ldC, selC);
 
-  modport reg_M1    (input wire load, sel, clock);
+  modport reg_D (input ldD, selD);
 
-  modport reg_M2    (input wire load, sel, clock);  
+  modport reg_INST (input ldINST, selINST);
 
-  modport reg_M     (input wire load, sel, clock);
- 
-  modport reg_X     (input wire load, sel, clock);
- 
-  modport reg_Y     (input wire load, sel, clock);
+  modport reg_M1 (input ldM1, selM1);
 
-  modport reg_XY    (input wire load, sel, clock);
+  modport reg_M2 (input ldM2, selM2);
 
-  modport reg_J1    (input wire load, sel, clock);
+  modport reg_M (input ldM, selM);
 
-  modport reg_J2    (input wire load, sel, clock);
+  modport reg_X (input ldX, selX);
 
-  modport reg_J     (input wire load, sel, clock);
+  modport reg_Y (input ldY, selY);
 
-  modport reg_INST  (input wire load, sel, clock);
+  modport reg_XY (input ldXY, selXY);
 
-  modport reg_PC    (input wire load, sel, clock);
+  modport reg_J1 (input ldJ1);
 
-  modport reg_INC   (input wire load, sel, clock);
+  modport reg_J2 (input ldJ2);
+
+  modport reg_J (input selJ);
+
+  modport reg_PC (input ldPC, selPC);
+
+  modport reg_INC (input ldINC, selINC);
+
+  modport reg_CCR (input ldCCR);
 
 endinterface
